c7c37af494dfb9485c1976f2d8380f49c33a879c
malgeon <nyangsun@gmail.com>
2022-04-03 20:14:08 +0900

    add - kotlin dsl 적용

---
+++ app/build.gradle.kts
@@ -0,0 +1,78 @@
+plugins {
+    id("com.android.application")
+    kotlin("android")
+    kotlin("kapt")
+//    id("dagger.hilt.android.plugin")
+    id("androidx.navigation.safeargs.kotlin")
+}
+
+
+android {
+    compileSdk = Versions.COMPILE_SDK
+
+    defaultConfig {
+        applicationId = "com.example.upa_app"
+        minSdk = Versions.MIN_SDK
+        targetSdk = Versions.TARGET_SDK
+        versionCode = Versions.versionCodeMobile
+        versionName = Versions.versionName
+
+        testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
+    }
+
+    buildFeatures {
+        viewBinding = true
+        dataBinding = true
+    }
+
+    compileOptions {
+        sourceCompatibility = JavaVersion.VERSION_1_8
+        targetCompatibility = JavaVersion.VERSION_1_8
+    }
+
+    kotlinOptions {
+        val options = this as org.jetbrains.kotlin.gradle.dsl.KotlinJvmOptions
+        options.jvmTarget = "1.8"
+    }
+}
+
+dependencies {
+    api(platform(project(":depconstraints")))
+    kapt(platform(project(":depconstraints")))
+    androidTestApi(platform(project(":depconstraints")))
+
+    implementation(Libs.APP_STARTUP)
+
+    // Date and time API for Java.
+    implementation(Libs.THREETENABP)
+    testImplementation(Libs.THREETENBP)
+
+    // UI
+    implementation(Libs.ACTIVITY_KTX)
+    implementation(Libs.APPCOMPAT)
+    implementation(Libs.FRAGMENT_KTX)
+    implementation(Libs.CONSTRAINT_LAYOUT)
+    implementation(Libs.MATERIAL)
+    implementation(Libs.NAVIGATION_FRAGMENT_KTX)
+    implementation(Libs.NAVIGATION_UI_KTX)
+    implementation(Libs.ROOM_KTX)
+    implementation(Libs.ROOM_RUNTIME)
+    kapt(Libs.ROOM_COMPILER)
+    implementation(Libs.GSON)
+
+//    // Dagger Hilt
+//    implementation(Libs.HILT_ANDROID)
+//    androidTestImplementation(Libs.HILT_TESTING)
+//    kapt(Libs.HILT_COMPILER)
+//    kaptAndroidTest(Libs.HILT_COMPILER)
+
+    // DataStore
+    implementation(Libs.DATA_STORE_PREFERENCES)
+    androidTestImplementation(Libs.DATA_STORE_PREFERENCES)
+
+    testImplementation("junit:junit:4.+")
+    androidTestImplementation("androidx.test.ext:junit:1.1.3")
+    androidTestImplementation("androidx.test.espresso:espresso-core:3.4.0")
+}
+
+apply(plugin = "com.google.gms.google-services")
--- build.gradle.kts
+++ build.gradle.kts
@@ -1,2 +1,31 @@
 import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 
+buildscript {
+    repositories {
+        google()
+        mavenCentral()
+    }
+    dependencies {
+        classpath("com.android.tools.build:gradle:7.0.4")
+        classpath("org.jetbrains.kotlin:kotlin-gradle-plugin:${Versions.KOTLIN}")
+        classpath("com.google.gms:google-services:${Versions.GOOGLE_SERVICES}")
+        classpath("androidx.navigation:navigation-safe-args-gradle-plugin:${Versions.NAVIGATION}")
+        classpath("com.google.dagger:hilt-android-gradle-plugin:${Versions.HILT_AGP}")
+//        classpath(Libs.Dagger.hiltGradlePlugin)
+
+        // NOTE: Do not place your application dependencies here; they belong
+        // in the individual module build.gradle.kts files
+    }
+}
+
+allprojects {
+    repositories {
+        google()
+        jcenter()
+
+    }
+}
+
+tasks.register("clean", Delete::class) {
+    delete(rootProject.buildDir)
+}
