bd31a4dbe123a4725015eacfdf4c7df26cfef3c9
malgeon <nyangsun@gmail.com>
2022-04-15 21:35:21 +0900

    add - firebase auth staging 처리

---
+++ app/src/main/java/com/example/upa_app/di/SignInModule.kt
@@ -0,0 +1,49 @@
+package com.example.upa_app.di
+
+import android.content.Context
+import com.example.upa_app.data.signin.datasources.AuthIdDataSource
+import com.example.upa_app.data.signin.datasources.AuthStateUserDataSource
+import com.example.upa_app.data.signin.datasources.RegisteredUserDataSource
+import dagger.Module
+import dagger.Provides
+import dagger.hilt.InstallIn
+import dagger.hilt.android.qualifiers.ApplicationContext
+import dagger.hilt.components.SingletonComponent
+import javax.inject.Singleton
+
+
+@InstallIn(SingletonComponent::class)
+@Module
+internal class SignInModule {
+
+    @Singleton
+    @Provides
+    fun provideRegisteredUserDataSource(
+        @ApplicationContext context: Context
+    ): RegisteredUserDataSource {
+        return StagingRegisteredUserDataSource(true)
+    }
+
+    @Singleton
+    @Provides
+    fun provideAuthStateUserDataSource(
+        @ApplicationContext context: Context,
+        notificationAlarmUpdater: NotificationAlarmUpdater
+    ): AuthStateUserDataSource {
+        return StagingAuthStateUserDataSource(
+            isRegistered = true,
+            isSignedIn = true,
+            context = context,
+            userId = "StagingTest",
+            notificationAlarmUpdater = notificationAlarmUpdater
+        )
+    }
+
+    @Singleton
+    @Provides
+    fun providesAuthIdDataSource(): AuthIdDataSource {
+        return object : AuthIdDataSource {
+            override fun getUserId() = "StagingTest"
+        }
+    }
+}
