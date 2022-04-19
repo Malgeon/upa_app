26b1f706bed15365938e4d62c75c7a74afa37a18
malgeon <nyangsun@gmail.com>
2022-04-19 14:28:57 +0900

    add - two pane photo fragment를 위한 작업 완료

--- presentation/src/main/java/com/example/upa_app/presentation/photo/PhotoFragment.kt
+++ presentation/src/main/java/com/example/upa_app/presentation/photo/PhotoFragment.kt
@@ -1,4 +1,4 @@
-package com.example.upa_app.presentation.randomphoto
+package com.example.upa_app.presentation.photo
 
 import android.os.Bundle
 import androidx.fragment.app.Fragment
@@ -9,13 +9,14 @@ import com.example.upa_app.presentation.R
 import dagger.hilt.android.AndroidEntryPoint
 
 @AndroidEntryPoint
-class PhotoTwoPaneFragment : Fragment() {
+class PhotoFragment : Fragment() {
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_photo_two_pane, container, false)
+        return inflater.inflate(R.layout.fragment_photo, container, false)
     }
+
 }
