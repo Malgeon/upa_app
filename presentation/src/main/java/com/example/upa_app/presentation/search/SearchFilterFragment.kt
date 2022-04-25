227fe60beaef89da43c4e26b71eb976e2c416daf
malgeon <nyangsun@gmail.com>
2022-04-25 13:57:20 +0900

    modify - search fragment 수정

--- presentation/src/main/java/com/example/upa_app/presentation/search/SearchFilterFragment.kt
+++ presentation/src/main/java/com/example/upa_app/presentation/search/SearchFilterFragment.kt
@@ -5,56 +5,20 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.fragment.app.viewModels
 import com.example.upa_app.presentation.R
+import com.example.upa_app.presentation.filters.FiltersFragment
+import com.example.upa_app.presentation.filters.FiltersViewModelDelegate
+import dagger.hilt.android.AndroidEntryPoint
 
-// TODO: Rename parameter arguments, choose names that match
-// the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-private const val ARG_PARAM1 = "param1"
-private const val ARG_PARAM2 = "param2"
+@AndroidEntryPoint
+class SearchFilterFragment : FiltersFragment() {
 
-/**
- * A simple [Fragment] subclass.
- * Use the [SearchFilterFragment.newInstance] factory method to
- * create an instance of this fragment.
- */
-class SearchFilterFragment : Fragment() {
-    // TODO: Rename and change types of parameters
-    private var param1: String? = null
-    private var param2: String? = null
+    private val viewModel: SearchViewModel by viewModels(
+        ownerProducer = { requireParentFragment() }
+    )
 
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        arguments?.let {
-            param1 = it.getString(ARG_PARAM1)
-            param2 = it.getString(ARG_PARAM2)
-        }
-    }
-
-    override fun onCreateView(
-        inflater: LayoutInflater, container: ViewGroup?,
-        savedInstanceState: Bundle?
-    ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_search_filter, container, false)
-    }
-
-    companion object {
-        /**
-         * Use this factory method to create a new instance of
-         * this fragment using the provided parameters.
-         *
-         * @param param1 Parameter 1.
-         * @param param2 Parameter 2.
-         * @return A new instance of fragment SearchFilterFragment.
-         */
-        // TODO: Rename and change types and number of parameters
-        @JvmStatic
-        fun newInstance(param1: String, param2: String) =
-            SearchFilterFragment().apply {
-                arguments = Bundle().apply {
-                    putString(ARG_PARAM1, param1)
-                    putString(ARG_PARAM2, param2)
-                }
-            }
+    override fun resolveViewModelDelegate(): FiltersViewModelDelegate {
+        return viewModel
     }
 }
--- presentation/src/main/java/com/example/upa_app/presentation/search/SearchViewModel.kt
+++ presentation/src/main/java/com/example/upa_app/presentation/search/SearchViewModel.kt
@@ -1,4 +1,134 @@
 package com.example.upa_app.presentation.search
 
-class SearchViewModel {
+import androidx.core.os.trace
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.example.upa_app.domain.search.LoadSearchFiltersUseCase
+import com.example.upa_app.domain.search.SessionSearchUseCase
+import com.example.upa_app.domain.search.SessionSearchUseCaseParams
+import com.example.upa_app.domain.settings.GetTimeZoneUseCase
+import com.example.upa_app.model.userdata.UserSession
+import com.example.upa_app.presentation.filters.FiltersViewModelDelegate
+import com.example.upa_app.presentation.signin.SignInViewModelDelegate
+import com.example.upa_app.shared.analytics.AnalyticsActions
+import com.example.upa_app.shared.analytics.AnalyticsHelper
+import com.example.upa_app.shared.result.Result
+import com.example.upa_app.shared.result.Result.Loading
+import com.example.upa_app.shared.result.successOr
+import com.example.upa_app.shared.util.TimeUtils
+import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.Job
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.flow.*
+import kotlinx.coroutines.launch
+import org.threeten.bp.ZoneId
+import javax.inject.Inject
+
+@HiltViewModel
+class SearchViewModel @Inject constructor(
+    private val analyticsHelper: AnalyticsHelper,
+    private val searchUseCase: SessionSearchUseCase,
+    getTimeZoneUseCase: GetTimeZoneUseCase,
+    loadFilterUseCase: LoadSearchFiltersUseCase,
+    signInViewModelDelegate: SignInViewModelDelegate,
+    filtersViewModelDelegate: FiltersViewModelDelegate
+) : ViewModel(),
+    SignInViewModelDelegate by signInViewModelDelegate,
+    FiltersViewModelDelegate by filtersViewModelDelegate {
+
+    private val _searchResults = MutableStateFlow<List<UserSession>>(emptyList())
+    val searchResults: StateFlow<List<UserSession>> = _searchResults
+
+    private val _isEmpty = MutableStateFlow(true)
+    val isEmpty: StateFlow<Boolean> = _isEmpty
+
+    private var searchJob: Job? = null
+
+    val timeZoneId: StateFlow<ZoneId> = flow {
+        if (getTimeZoneUseCase(Unit).successOr(true)) {
+            emit(TimeUtils.CONFERENCE_TIMEZONE)
+        } else {
+            emit(ZoneId.systemDefault())
+        }
+    }.stateIn(viewModelScope, SharingStarted.Lazily, ZoneId.systemDefault())
+
+    private var textQuery = ""
+
+    // Override because we also want to show result count when there's a text query.
+    private val _showResultCount = MutableStateFlow(false)
+    override val showResultCount: StateFlow<Boolean> = _showResultCount
+
+    init {
+        // Load filters
+        viewModelScope.launch {
+            setSupportedFilters(loadFilterUseCase(Unit).successOr(emptyList()))
+        }
+
+        // Re-execute search when selected filters change
+        viewModelScope.launch {
+            selectedFilters.collect {
+                executeSearch()
+            }
+        }
+
+        // Re-execute search when signed in user changes.
+        // Required because we show star / reservation status.
+        viewModelScope.launch {
+            userInfo.collect {
+                executeSearch()
+            }
+        }
+    }
+
+    fun onSearchQueryChanged(query: String) {
+        val newQuery = query.trim().takeIf { it.length >= 2 } ?: ""
+        if (textQuery != newQuery) {
+            textQuery = newQuery
+            analyticsHelper.logUiEvent("Query: $newQuery", AnalyticsActions.SEARCH_QUERY_SUBMIT)
+            executeSearch()
+        }
+    }
+
+    private fun executeSearch() {
+        // Cancel any in-flight searches
+        searchJob?.cancel()
+
+        val filters = selectedFilters.value
+        if (textQuery.isEmpty() && filters.isEmpty()) {
+            clearSearchResults()
+            return
+        }
+
+        searchJob = viewModelScope.launch {
+            // The user could be typing or toggling filters rapidly. Giving the search job
+            // a slight delay and cancelling it on each call to this method effectively debounces.
+            delay(500)
+            trace("search-path-viewmodel") {
+                searchUseCase(
+                    SessionSearchUseCaseParams(userIdValue, textQuery, filters)
+                ).collect {
+                    processSearchResult(it)
+                }
+            }
+        }
+    }
+
+    private fun clearSearchResults() {
+        _searchResults.value = emptyList()
+        // Explicitly set false to not show the "No results" state
+        _isEmpty.value = false
+        _showResultCount.value = false
+        resultCount.value = 0
+    }
+
+    private fun processSearchResult(searchResult: Result<List<UserSession>>) {
+        if (searchResult is Loading) {
+            return // avoid UI flickering
+        }
+        val sessions = searchResult.successOr(emptyList())
+        _searchResults.value = sessions
+        _isEmpty.value = sessions.isEmpty()
+        _showResultCount.value = true
+        resultCount.value = sessions.size
+    }
 }
