af1f55f896d50ea48e085f43d4f2d7f8b4c7c1f4
malgeon <nyangsun@gmail.com>
2022-04-29 18:04:30 +0900

    modify - unsplash photo data source inject 추가

--- data/src/main/java/com/example/upa_app/data/unsplash/UnsplashPhotoDataSource.kt
+++ data/src/main/java/com/example/upa_app/data/unsplash/UnsplashPhotoDataSource.kt
@@ -1,16 +1,23 @@
 package com.example.upa_app.data.unsplash
 
 import com.example.upa_app.model.unsplash.UnsplashPhotoData
+import javax.inject.Inject
 
 interface UnsplashPhotoDataSource {
     fun getRemotePhotoData(): UnsplashPhotoData?
 }
 
-class DefaultUnsplashPhotoDataSource : UnsplashPhotoDataSource {
+class DefaultUnsplashPhotoDataSource @Inject constructor(
+
+) : UnsplashPhotoDataSource {
 
 
     override fun getRemotePhotoData(): UnsplashPhotoData? {
         TODO("Not yet implemented")
     }
+
+    private fun
+
+
 
 }
