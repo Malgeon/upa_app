dae650ccb80f48f89a4c31e1a8a248bc1ac2d709
malgeon <nyangsun@gmail.com>
2022-05-08 20:00:40 +0900

    modify - 캐싱 관련 함수 변경

--- data/src/main/java/com/example/upa_app/data/unsplash/UnsplashPhotoRepository.kt
+++ data/src/main/java/com/example/upa_app/data/unsplash/UnsplashPhotoRepository.kt
@@ -19,112 +19,6 @@ class UnsplashPhotoRepository @Inject constructor(
     private val remoteDataSource: UnsplashPhotoDataSource,
     private val unsplashDao: UnsplashDao
 ) {
-
-    // In-memory cache of the photo data
-    private var unsplashPhotoDataCache: UnsplashPhotoData? = null
-
-    var latestException: Exception? = null
-        private set
-    var latestUpdateSource: UpdateSource = UpdateSource.NONE
-
-    // Using a SharedFlow instead of StateFlow as there isn't an initial value to be emitted
-    private val dataLastUpdatedFlow = MutableSharedFlow<Long>(replay = 1)
-    val dataLastUpDatedObservable: Flow<Long> = dataLastUpdatedFlow
-
-    // Prevents multiple consumers requesting data at the same time
-    private val loadPhotoDataLock = Any()
-
-    @WorkerThread
-    fun refreshCacheWithRemotePhotoData() {
-        val unsplashPhotoData = try {
-            remoteDataSource.getRemotePhotoData()
-        } catch (e: IOException) {
-            latestException = e
-            throw e
-        }
-        if (unsplashPhotoData == null) {
-            val e = Exception("Remote returned no photo data")
-            latestException = e
-            throw e
-        }
-
-        // Network data success!
-        // Update cache
-        synchronized(loadPhotoDataLock) {
-            unsplashPhotoDataCache = unsplashPhotoData
-            populatePhotoData(unsplashPhotoData)
-        }
-
-        // Update meta
-        latestException = null
-        dataLastUpdatedFlow.tryEmit(System.currentTimeMillis())
-        latestUpdateSource = UpdateSource.NETWORK
-        latestException = null
-    }
-
-    fun getOfflinePhotoData(): UnsplashPhotoData {
-        synchronized(loadPhotoDataLock) {
-            val offlineData = unsplashPhotoDataCache ?: getCacheOrLocalDataAndPopulatePhoto()
-            unsplashPhotoDataCache = offlineData
-            return offlineData
-        }
-    }
-
-    private fun getCacheOrLocalDataAndPopulatePhoto(): UnsplashPhotoData {
-        val photoData = getCacheOrLocalData()
-
-    }
-
-    private fun getCacheOrLocalData(): UnsplashPhotoData {
-        // First, try the local cache:
-        var photoData =
-    }
-
-    open fun populatePhotoData(photoData: UnsplashPhotoData) {
-        val photoEntity = photoData.unsplashPhotos.map { unsplashPhoto ->
-            UnsplashEntity(
-                id = unsplashPhoto.id,
-                urls = unsplashPhoto.urls,
-                user = unsplashPhoto.user
-            )
-        }
-        unsplashDao.insertAll(photoEntity)
-    }
-
-    @WorkerThread
-    fun refreshCacheWithRemotePhotoDataFlow(): Flow<Result<T>> =
-        flow {
-            val unsplashPhotoData = try {
-                remoteDataSource.getRemotePhotoData()
-            } catch (e: IOException) {
-                emit(Result.Error(e))
-            }
-            if (unsplashPhotoData == null) {
-                val e = Exception("Remote returned no photo data")
-                latestException = e
-                emit(Result.Error(e))
-            }
-
-            // Network data success!
-            // Update cache
-            synchronized(loadPhotoDataLock) {
-                unsplashPhotoDataCache = unsplashPhotoData
-                populatePhotoData(unsplashPhotoData)
-            }
-
-            // Update meta
-            latestException = null
-            dataLastUpdatedFlow.tryEmit(System.currentTimeMillis())
-            latestUpdateSource = UpdateSource.NETWORK
-            latestException = null
-        }
-
-
-}
-
-class SampleUnsplashPhotoRepository {
-
-
     // In-memory cache of the conference data
     private var conferenceDataCache: ConferenceData? = null
 
@@ -210,23 +104,7 @@ class SampleUnsplashPhotoRepository {
                 speakers = session.speakers.joinToString { it.name }
             )
         }
-        appDatabase.sessionFtsDao().insertAll(sessionFtsEntities)
-        val speakers = conferenceData.speakers.map {
-            SpeakerFtsEntity(
-                speakerId = it.id,
-                name = it.name,
-                description = it.biography
-            )
-        }
-        appDatabase.speakerFtsDao().insertAll(speakers)
-        val codelabs = conferenceData.codelabs.map {
-            CodelabFtsEntity(
-                codelabId = it.id,
-                title = it.title,
-                description = it.description
-            )
-        }
-        appDatabase.codelabFtsDao().insertAll(codelabs)
+        unsplashDao.sessionFtsDao().insertAll(sessionFtsEntities)
     }
 
     open fun getConferenceDays(): List<ConferenceDay> = TimeUtils.ConferenceDays
